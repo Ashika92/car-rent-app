056d0e3db02edd9e1975f71e8709bc13dcaa3494
ASHIKA <ashikajaleel@gmail.com>
2025-10-28 04:38:27 +0530

    Initial commit

---
+++ car-rent-app/src/main.jsx
@@ -0,0 +1,15 @@
+// main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App"; // your main App component
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
